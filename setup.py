9cc4325c933e7538d0863730d920c870fa34ab71
chriddyp <chris@plot.ly>
2015-07-13 20:36:18 -0400

    new v

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='publisher',
-    version='0.1',
+    version='0.2',
     author='chris p',
     author_email='chris@plot.ly',
     packages=['publisher'],
